67ad8e70f892c8e2c200263dabf0888237281e96
AnahitPetrosyan <anahit@xamlab.com>
2019-12-30 12:39:20 +0400

    creating standard calc constraintlayout

---
+++ app/src/main/java/com/example/studyingmvvm/StandardCalcActivity.kt
@@ -0,0 +1,12 @@
+package com.example.studyingmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StandardCalcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_standard_calc)
+    }
+}
